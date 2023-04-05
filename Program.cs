0a6588ab2c4cb08062872485e9b95f8ab1ea3bda
Philip Karastoyanov <philipgg11@gmail.com>
2023-04-05 22:46:08 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace SpaceLaunch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine(args[0]);
+        }
+    }
+}
