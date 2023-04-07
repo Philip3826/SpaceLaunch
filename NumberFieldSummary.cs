5dd13b58a4ce37fc65181dc74480d3f78ef328f6
Philip Karastoyanov <philipgg11@gmail.com>
2023-04-07 18:40:12 +0300

    added classes that will hold summary info for the fields of the csv

---
+++ NumberFieldSummary.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace SpaceLaunch
+{
+    class NumberFieldSummary
+    {
+        public string FieldName { get; private set; } = "";
+        public int MinimalValue { get; private set; } = int.MinValue;
+        public int MaximumValue { get; private set; } = int.MaxValue;
+        public double AverageValue { get; private set; } = 0;
+        public double MedianValue { get; private set; } = 0;
+        public int BestLaunchDay { get; private set; } = 0;
+        private int upperBound = int.MaxValue;
+        private int lowerBound = int.MinValue;
+        private List<int> values;
+        public List<int> Values => values;
+        public NumberFieldSummary(List<int> values, string name, int lowerBound, int upperBound )
+        {
+            this.values = values;
+            this.FieldName = name;
+            this.lowerBound = lowerBound;
+            this.upperBound = upperBound;
+            CalculateSummary();
+        }
+
+        private void CalculateSummary()
+        {
+            List<int> copyValues = values;
+            copyValues.Sort();
+            MinimalValue = copyValues[0];
+            MaximumValue = copyValues.Last();
+            AverageValue = (double)(copyValues.Sum() / copyValues.Count);
+
+            if (copyValues.Count % 2 == 0)
+            {
+                MedianValue = (double)(copyValues[copyValues.Count / 2] + copyValues[(copyValues.Count/ 2) -1]);
+            }
+            else MedianValue = copyValues[copyValues.Count / 2];
+        }
+       
+    }
+}
---
+++ StringFieldSummary.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SpaceLaunch
+{
+    class StringFieldSummary
+    {
+        public string FieldName { get; private set; } = "";
+        public int BestLaunchDate { get; private set; } = 0;
+        private List<string> values;
+
+        public StringFieldSummary(List<string> values, string name)
+        {
+            this.values = values;
+            this.FieldName = name;
+            CalculateBestLaunchDate();
+        }
+        
+        private void CalculateBestLaunchDate()
+        {
+            switch (FieldName)
+            {
+                case "Lightning":
+                    {
+                        for(int i = 0; i < values.Count; i++)
+                        {
+                            if (values[i] == "Yes") BestLaunchDate = i + 1;
+                            break;
+                        }
+                    }break;
+                case "Clouds":
+                    {
+                        for (int i = 0; i < values.Count; i++)
+                        {
+                            if (values[i] != "Cumulus" && values[i] != "Nimbus") BestLaunchDate = i + 1;
+                            break;
+                        }
+                    }break;
+            }
+        }
+    }
+}
