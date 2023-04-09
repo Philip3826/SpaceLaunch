5dbefa5678fd46d8c7b883e77cd3d376a5f09adb
Philip3826 <philipgg11@gmail.com>
2023-04-09 20:30:48 +0300

    started work on email sending (not working yet)

--- Program.cs
+++ Program.cs
@@ -1,6 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-
+using System.Net;
+using System.Net.Mail;
 namespace SpaceLaunch
 {
     class Program
@@ -12,8 +12,32 @@ namespace SpaceLaunch
             //some changes here 
             CsvWriter writer = new CsvWriter(reader.GetRawDataRows, "C:\\Users\\Radena\\Source\\Repos\\SpaceLaunch\\WeatherReport.csv");
             DataAnalyzer dataAnalyzer = new DataAnalyzer(reader.GetRawDataColumns.GetRange(1, reader.GetRawDataColumns.Count - 1));
-            Console.WriteLine(dataAnalyzer.CalculateBestLaunchDay());
-            
+            int day = dataAnalyzer.CalculateBestLaunchDay();
+
+            MailMessage mailMessage = new MailMessage();
+            mailMessage.From = new MailAddress("senderLaunchDate@mail.bg");
+            mailMessage.To.Add(new MailAddress("receiverLaunchDate@mail.bg"));
+            mailMessage.Subject = "Test weather report";
+            mailMessage.Body = "Best Launch Day: " + day.ToString();
+
+            // Attachment attachment = new Attachment("C:\\Users\\Radena\\Source\\Repos\\SpaceLaunch\\WeatherReport.csv");
+            // mailMessage.Attachments.Add(attachment);
+            try
+            {
+                SmtpClient smtpClient = new SmtpClient();
+                smtpClient.Host = "smtp.mail.bg";
+                smtpClient.Port = 25;
+                smtpClient.EnableSsl = true;
+                smtpClient.Credentials = new NetworkCredential("senderlaunchdate@mail.bg", "014426045fFF");
+                smtpClient.Send(mailMessage);
+                Console.WriteLine("Sent mail!");
+            }
+            catch (Exception e)
+            {
+
+                Console.WriteLine(e.ToString());
+
+            }
         }
     }
 }
